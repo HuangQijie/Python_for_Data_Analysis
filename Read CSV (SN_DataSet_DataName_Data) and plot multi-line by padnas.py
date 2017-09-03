afb7457da227b0bb34338b6ed83e4529785c0cc1
USER-E972PJGQCH\Administrator <andrewhuang177@gmail.com>
2017-09-03 22:44:57 +0800

    2017年9月3日

--- Read CSV (SN_DataSet_DataName_Data) and plot multi-line by padnas.py
+++ Read CSV (SN_DataSet_DataName_Data) and plot multi-line by padnas.py
@@ -11,16 +11,22 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import csv
 import pandas as pd
+from pandas import Series, DataFrame
+import re
 
 
-#%%
+#SN_Index=0
+#DataSet_Index=5
+#DataName_Index=8
+#DataValue_Index=9
+
+
 SN_Index=0
-DataSet_Index=5
-DataName_Index=8
-DataValue_Index=9
+DataSet_Index=1
+DataName_Index=2
+DataValue_Index=3
 
-
-data_read=pd.read_csv('C:\\Users\\Andy.Huang\\Desktop\\tttt.csv')
+data_read=pd.read_csv('D:\\GitHub_data\\Python_for_Data_Analysis\\bbb.csv')
 
 Column_Name=data_read.columns
 SN_Name=Column_Name[SN_Index]
@@ -29,66 +35,44 @@ DataName_Name=Column_Name[DataName_Index]
 DataValue_Name=Column_Name[DataValue_Index]
 
 
-data_read=data_read.set_index([SN_Name,DataSet_Name])
+#data_read=data_read.set_index([SN_Name,DataSet_Name])
 
-data=pd.DataFrame(data_read,columns=[ DataName_Name,DataValue_Name])
+data=pd.DataFrame(data_read,columns=[ SN_Name,DataSet_Name,DataName_Name,DataValue_Name])
 
-DataName_frame=data[DataName_Name]
-
-DataName_frame.str.
-
-#DataName_Series=data[DataName_Name]
-
-
-#dt=SN_DataSet_DataName_DataValue_Dict()
-
-#for row in data:
-#    print(row)
+DataName_DataFrame=data[DataName_Name]
+#pattern='[a-zA-Z_:\[\]]+([0-9]+)' # screen out the channel number
+pattern='.+\-([0-9]+)'
+DataName_DataFrame=DataName_DataFrame.str.extract(pattern,flags=re.IGNORECASE)
 
 
+del data[DataName_Name]
+data.insert(2,DataName_Name,DataName_DataFrame)
+data=data.dropna()#delete row whose dataname is NA
+#data=data.set_index([SN_Name,DataSet_Name])
+ 
+SN_list=data.drop_duplicates([SN_Name])[SN_Name]#get SN list
+DataSet_list=data.drop_duplicates([DataSet_Name])[DataSet_Name]#get dataname list
 
-#    #%%
-#    '''
-#    Modify the data in below
-#    '''
-##    if str.isnumeric(row[DataName_Index][len(row[DataName_Index])-4:len(row[DataName_Index])-1]):
-##        chan=row[DataName_Index][len(row[DataName_Index])-4:len(row[DataName_Index])-1]
-#    #chan=row[DataName_Index].split('-')[1]
-#    #%%
-#    '''
-#        Data is transfered into nested dict
-#    '''
-#    dt.append_DataName_DataValue_data(row[SN_Index],row[DataSet_Index],row[DataName_Index],float(row[DataValue_Index]))
-#    #dt.append_DataName_DataValue_data(row[SN_Index],row[DataSet_Index],float(chan),float(row[DataValue_Index]))
-
-#%% Start plotting
-#for SN in dt:
-##    print(SN)
-#    for DataSet in dt[SN]:
-##        print('\t'+DataSet)
-#        plotdata={}
-#        #plotdata_sorted={}
-#        for DataName in dt[SN][DataSet]:
-##            print('\t\t'+DataName,end='\t')
-##            print(dt[SN][DataSet][DataName])
-#            plotdata[DataName]=dt[SN][DataSet][DataName]
-##%%Sort the Dataname , because dataname is string type and usually not sorted
-#        Name=[]
-#        Value=[]
-#        for k in sorted(plotdata.keys()):# Sort by keys
-#            Name.append(k)
-#            Value.append(plotdata[k])
-##%%Sort end here
-#        #for item
-#        #print (plotdata.items())
-#
-#        plt.plot(Name,Value,label=SN+'_'+DataSet)
+#for sn in SN_list:
+#    for dataset in DataSet_list:
+#        plotdata=data[data[DataSet_Name]==dataset][data[SN_Name]==sn]
+#        plotdata.sort_index(by=DataName_Name)
+#        pass
+#        plt.plot(plotdata[DataName_Name],plotdata[DataValue_Name],label=sn+'_'+dataset)
 #        plt.legend(loc='upper left',bbox_to_anchor=(1,1))
-#
-#
-#plt.grid()
-#plt.xlabel('Channel')
-#plt.ylabel('Frequency Delta (GHz)')
+
+
+for sn in SN_list:
+    plotdata=data[data[SN_Name]==sn]
+    plotdata=plotdata.apply(pd.to_numeric, errors='ignore')
+    plotdata=plotdata.sort_values(by=DataName_Name)
+    pass
+    plt.plot(plotdata[DataName_Name],plotdata[DataValue_Name],label=sn)
+    plt.legend(loc='best',bbox_to_anchor=(1,1))
+
+plt.grid()
+plt.xlabel('Channel')
+plt.ylabel('Frequency Delta (GHz)')
 #plt.autoscale(True,True,True)
 
 
