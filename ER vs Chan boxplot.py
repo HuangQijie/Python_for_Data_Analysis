50add726118886f76c8448b5945c70797f897cff
HuangQijie <andrewhuang177@gmail.com>
2017-09-05 17:03:49 +0800

    2017.9.5 add some souce data

--- ER vs Chan boxplot.py
+++ ER vs Chan boxplot.py
@@ -14,11 +14,12 @@ from pandas import Series, DataFrame
 import re
 
 SN_Index=0
-Chan_Index=1
-ER_Index=2
+Chan_Index=9
+ER_Index=10
 #DataValue_Index=3
 
-data_read=pd.read_csv('D:\\GitHub_data\\Python_for_Data_Analysis\\bbb.csv')
+#data_read=pd.read_csv('D:\\GitHub_data\\Python_for_Data_Analysis\\bbb.csv')
+data_read=pd.read_csv('C:\\Users\\Andy.Huang\\Desktop\\TSFP Negative chirp.csv')
 
 Column_Name=data_read.columns
 SN_Name=Column_Name[SN_Index]
@@ -28,10 +29,17 @@ ER_Name=Column_Name[ER_Index]
 
 data=pd.DataFrame(data_read,columns=[Chan_Name,ER_Name])
 Chan_Series=data.drop_duplicates(Chan_Name)[Chan_Name]
-Chan_Series=Chan_Series.Sort()
+Chan_Series=Chan_Series.sort_values()
 
 ER_dataframe=DataFrame()
 for chan in Chan_Series:
     ER_Series=data[data[Chan_Name]==chan][ER_Name]
-    ER_dataframe[chan]=ER_Series
+    #ER_Series=ER_Series.rename(columns={ER_Name:str(chan)})
+    #ER_dataframe=ER_dataframe.assign(chan=ER_Series.values,ignore_index=True)
+    ER_dataframe=pd.concat([ER_dataframe,pd.DataFrame({chan:ER_Series})],axis=1)
 
+
+ER_dataframe.boxplot()
+plt.xlabel('Channel')
+plt.ylabel('ER(dB)')
+plt.title('TSFP Negative chirp ER distribution')
